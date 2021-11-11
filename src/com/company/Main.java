00fd7e9fa0de2b7c2591af065a4c03bcfbee0939
Aizhan <azhimgereyeva@mail.ru>
2021-11-11 09:56:27 +0600

    initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
-
+import java.util.Collection;
+import java.util.PriorityQueue;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        PriorityQueue<String> queue1 = new PriorityQueue<String>();
+        queue1.offer("alma");
+        queue1.offer("banan");
+        queue1.offer("zhuzim");
+        queue1.offer("persik");
+        System.out.println("Priority queue using Comparable:");
+        while (queue1.size() > 0) {
+            System.out.print(queue1.remove() + " ");
+        }
     }
 }
